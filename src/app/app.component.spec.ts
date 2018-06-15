0d5394e0cd44e359c932e3ac5099d6ba542aeea6
Michael Nguyen <duyl3nguy3n@gmail.com>
2018-06-15 01:47:28 -0400

    fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './app-routing.module';
+import { LibModule } from './lib/lib.module';
+import { HomeModule } from './modules/home/home.module';
+import { SystemModule } from './modules/system/system.module';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AppRoutingModule,
+        LibModule,
+        HomeModule,
+        SystemModule
       ],
       declarations: [
         AppComponent
